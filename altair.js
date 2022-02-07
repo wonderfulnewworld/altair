b932659b2cb805b308d8c1eaf76336a177f8b1c7
wonderfulnewworld <99152399+wonderfulnewworld@users.noreply.github.com>
2022-02-06 17:06:09 -0800

    Uploading to git.

---
+++ altair.js
@@ -0,0 +1,15 @@
+import { altair } from "./module/config.js";
+import AltairItemSheet from "./module/sheets/AltairItemSheet.js";
+import AltairPlayerSheet from "./module/sheets/AltairPlayerSheet.js"
+
+Hooks.once("init", function() {
+    console.log("altair | Initializing Altair System");
+
+    CONFIG.altair = altair;
+    
+    Items.unregisterSheet("core", ItemSheet);
+    Items.registerSheet("altair", AltairItemSheet, { makeDefault: true });
+
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet("altair", AltairPlayerSheet, { makeDefault: true });
+});
