580fe159eea082438609abc3c6806fc65cbbd2b5
wonderfulnewworld <99152399+wonderfulnewworld@users.noreply.github.com>
2022-02-06 17:06:42 -0800

    uploading config

---
+++ module/config.js
@@ -0,0 +1,59 @@
+export const altair = {}
+
+altair.characterInfo = {
+    name: "altair.characterInfo.name",
+    level: "altair.characterInfo.level",
+    health: "altair.characterInfo.health",
+    healthRegen: "altair.characterInfo.healthRegen",
+    stamina: "altair.characterInfo.stamina",
+    staminaRegen: "altair.characterInfo.staminaRegen",
+    focus: "altair.characterInfo.focus",
+    focusRegen: "altair.characterInfo.focusRegen",
+    drive: "altair.characterInfo.drive",
+    purity: "altair.characterInfo.purity",
+    actionCap: "altair.characterInfo.actionCap",
+    actionRegen: "altair.characterInfo.actionRegen",
+    break: "altair.characterInfo.break",
+    exp: "altair.characterInfo.exp",
+    lifetimeExperience: "altair.characterInfo.lifetimeExperience",
+    movement: "altair.characterInfo.movement",
+    speed: "altair.characterInfo.speed",
+    money: "altair.characterInfo.money",
+    size: "altair.characterInfo.size",
+    bio: "altair.characterInfo.bio",
+    title: "altair.characterInfo.title",
+    personalSkill: "altair.characterInfo.personalSkill",
+    element: "altair.characterInfo.element",
+    drop: "altair.characterInfo.drop",
+    strength: "altair.characterInfo.strength",
+    magic: "altair.characterInfo.magic",
+    defense: "altair.characterInfo.defense",
+    spirit: "altair.characterInfo.spirit",
+    skill: "altair.characterInfo.skill",
+    charm: "altair.characterInfo.charm"
+}
+
+altair.elements = {
+    fire: "altair.elements.fire",
+    ice: "altair.elements.ice",
+    thunder: "altair.elements.thunder",
+    wind: "altair.elements.wind",
+    ground: "altair.elements.ground",
+    neutral: "altair.elements.neutral",
+    light: "altair.elements.light",
+    holy: "altair.elements.holy",
+    dark: "altair.elements.dark",
+    abnormal: "altair.elements.abnormal"
+}
+
+altair.attackTypes = {
+    none: "altair.attackTypes.none",
+    all: "altair.attackTypes.all",
+    melee: "altair.attackTypes.melee",
+    martial: "altair.attackTypes.martial",
+    projectile: "altair.attackTypes.projectile",
+    firearm: "altair.attackTypes.firearm",
+    magic: "altair.attackTypes.magic",
+    monster: "altair.attackTypes.monster",
+    abnormal: "altair.attackTypes.abnormal"
+}
