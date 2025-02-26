fc21edf92573def230a95c32637b4aa62cb7f927
wonderfulnewworld <shinsekaihibiki@gmail.com>
2025-02-26 01:25:34 -0800

    BUGS BEGONE
    BUGS NOMORE

--- module/sheets/AltairPlayerSheet.mjs
+++ module/sheets/AltairPlayerSheet.mjs
@@ -16,64 +16,29 @@ export default class AltairPlayerSheet extends api.HandlebarsApplicationMixin(
         //return 'systems/altair/templates/sheets/$(this.item.type}-sheet.html';
     }
     static PARTS = {
-        template: "systems/altair/templates/sheets/player-sheet.hbs"
+        body: {
+            template: "systems/altair/templates/sheets/player-sheet.hbs",
+            scrollable: [""]
+        }
     }
     /** @override */
     static DEFAULT_OPTIONS = {
-        classes: ['altair', 'sheet', 'actor'],
+        classes: ['altair', 'sheet', 'actor', 'standard-form'],
         position: {
-            width: 600
+            height: 800,
+            width: 700
         },
-        window: {
-            controls: [
-                {
-                    action: "configurePrototypeToken",
-                    icon: "fa-solid fa-user-circle",
-                    label: "TOKEN.TitlePrototype",
-                    ownership: "OWNER"
-                },
-                {
-                    action: "showPortraitArtwork",
-                    icon: "fa-solid fa-image",
-                    label: "SIDEBAR.CharArt",
-                    ownership: "OWNER"
-                },
-                {
-                    action: "showTokenArtwork",
-                    icon: "fa-solid fa-image",
-                    label: "SIDEBAR.TokenArt",
-                    ownership: "OWNER"
-                }
-            ]
-        },
+        window: { resizable: true },
         form: {
-            submitOnChange: true
-        },
-        actions: {
-            configurePrototypeToken: AltairPlayerSheet.#onConfigurePrototypeToken,
-            showPortraitArtwork: AltairPlayerSheet.#onShowPortraitArtwork,
-            showTokenArtwork: AltairPlayerSheet.#onShowTokenArtwork,
+            submitOnChange: true,
+            closeOnSubmit: false,
         }
     }
-
-    //get title() {
-    //    return `My Module: ${game.i18n.localize(this.options.window.title)}`;
-    //}
-    /*
-    return foundry.utils.mergeObject(super.defaultOptions, {
-        classes: ['altair', 'sheet', 'actor'],
-        width: 1000,
-        height: 700,
-        scrollY: [".altair-sheet"],
-        tabs: [
-            {
-                navSelector: '.sheet-tabs',
-                contentSelector: '.sheet-body',
-                initial: 'features',
-            },
-        ],
-    });
-}*/
+    
+    /*async _preSyncPartState() {
+        console.log(super._preSyncPartState());
+        return;
+    }*/
 
     async _prepareContext(options) {
         //console.log("test")
@@ -102,98 +67,10 @@ export default class AltairPlayerSheet extends api.HandlebarsApplicationMixin(
         return sheetData;
     }
 
-    /**
-     * The Actor document managed by this sheet.
-     * @type {ClientDocument}
-     */
-    get actor() {
-        return this.document;
-    }
-
-    /* -------------------------------------------- */
-
-    /**
-     * If this sheet manages the ActorDelta of an unlinked Token, reference that Token document.
-     * @type {TokenDocument|null}
-     */
-    get token() {
-        return this.document.token || null;
-    }
-
-    /** @override */
-    _getHeaderControls() {
-        const controls = this.options.window.controls;
-
-        // Portrait image
-        const img = this.actor.img;
-        if (img === CONST.DEFAULT_TOKEN) controls.findSplice(c => c.action === "showPortraitArtwork");
-
-        // Token image
-        const pt = this.actor.prototypeToken;
-        const tex = pt.texture.src;
-        if (pt.randomImg || [null, undefined, CONST.DEFAULT_TOKEN].includes(tex)) {
-            controls.findSplice(c => c.action === "showTokenArtwork");
-        }
-        return controls;
-    }
-
-    /* -------------------------------------------- */
-
-    //async _renderHTML(context, options) {
-    //    return `<p>TESTING</p>`;
-    //}
-
-    _replaceHTML(result, content, options) {
-        content.insertAdjacentHTML("beforeend", result);
-    }
-
-    /* -------------------------------------------- */
-    /* -------------------------------------------- */
-    /*  Event Listeners and Handlers                */
-    /* -------------------------------------------- */
-
-    /**
-     * Handle header control button clicks to render the Prototype Token configuration sheet.
-     * @this {ActorSheetV2}
-     * @param {PointerEvent} event
-     */
-    static #onConfigurePrototypeToken(event) {
-        event.preventDefault();
-        const renderOptions = {
-            left: Math.max(this.position.left - 560 - 10, 10),
-            top: this.position.top
-        };
-        new CONFIG.Token.prototypeSheetClass(this.actor.prototypeToken, renderOptions).render(true);
-    }
-
-    /* -------------------------------------------- */
-
-    /**
-     * Handle header control button clicks to display actor portrait artwork.
-     * @this {ActorSheetV2}
-     * @param {PointerEvent} event
-     */
-    static #onShowPortraitArtwork(event) {
-        const { img, name, uuid } = this.actor;
-        new ImagePopout(img, { title: name, uuid: uuid }).render(true);
-    }
-
-    /* -------------------------------------------- */
-
-    /**
-     * Handle header control button clicks to display actor portrait artwork.
-     * @this {ActorSheetV2}
-     * @param {PointerEvent} event
-     */
-    static #onShowTokenArtwork(event) {
-        const { prototypeToken, name, uuid } = this.actor;
-        new ImagePopout(prototypeToken.texture.src, { title: name, uuid: uuid }).render(true);
-    }
-
     // choices JS stuff
     //I'll try fixing this after..
-    /*activateListeners(html) {
-        super.activateListeners(html);
+    /*_onRender(context,options) {
+        const html = $(this.element)
 
         // locates the choice by its id
         const selects = html.find('.choice-selector');
